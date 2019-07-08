1a271fcd6c89398f0d06d41f14068e8d83fb233e
Pedram <foroughi.pedram@gmail.com>
2019-07-08 12:13:22 +0300

    Update GuessPresenter.kt
    Added functionality to the guess function which now gusses a random food from a predefined array

--- app/src/main/java/com/techspark/whateats/GuessPresenter.kt
+++ app/src/main/java/com/techspark/whateats/GuessPresenter.kt
@@ -1,8 +1,19 @@
 package com.techspark.whateats
 
-class GuessPresenter (view: Contract.View) : Contract.Presenter{
+import android.content.Context
+import kotlin.random.Random
 
+class GuessPresenter (private val view: Contract.View, private val context: Context) : Contract.Presenter{
+
+    private val foods: Array<String> = context.resources.getStringArray(R.array.foods)
+
+    /**
+     * Picks a random number and selects the respective index of food array
+     */
     override fun guess() {
 
+        val rand = Random(System.currentTimeMillis()).nextInt(0,foods.size)
+
+        view.guess(foods[rand])
     }
 }
