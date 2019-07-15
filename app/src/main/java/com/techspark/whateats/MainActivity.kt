4b1b1f248972d7ade4f15a5a4ba2ab5cc84e47bd
Pedram <foroughi.pedram@gmail.com>
2019-07-15 15:48:53 +0300

    Update Main Activity
    Added banner to Main Activity

--- app/src/main/java/com/techspark/whateats/MainActivity.kt
+++ app/src/main/java/com/techspark/whateats/MainActivity.kt
@@ -2,11 +2,19 @@ package com.techspark.whateats
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.gms.ads.AdRequest
+import com.google.android.gms.ads.MobileAds
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        MobileAds.initialize(this,resources.getString(R.string.ad_id))
+
+        val adReq = AdRequest.Builder().addTestDevice(AdRequest.DEVICE_ID_EMULATOR).build()
+        ad_view.loadAd(adReq)
+
     }
 }
