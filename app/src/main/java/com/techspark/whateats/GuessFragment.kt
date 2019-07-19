b1f38e4d8159d46b626ed1193c8f5f0f989bea2e
Pedram <foroughi.pedram@gmail.com>
2019-07-19 08:48:51 +0300

    Update Guess Fragment
    Updated onStartGuessing and onStopGuessing functions to enable/disable guess button

--- app/src/main/java/com/techspark/whateats/GuessFragment.kt
+++ app/src/main/java/com/techspark/whateats/GuessFragment.kt
@@ -1,15 +1,12 @@
 package com.techspark.whateats
 
 
-import android.os.Build
 import android.os.Bundle
-import android.speech.tts.TextToSpeech
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import kotlinx.android.synthetic.main.fragment_guess.*
-import java.util.*
 
 
 /**
@@ -30,10 +27,12 @@ class GuessFragment : Fragment(), Contract.View {
 
     override fun onStartGuessing() {
 
+        button_guess.isEnabled = false
     }
 
     override fun onStopGuessing() {
 
+        button_guess.isEnabled = true
     }
 
     override fun onCreateView(
@@ -49,7 +48,7 @@ class GuessFragment : Fragment(), Contract.View {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        image_button_guess.setOnClickListener {
+        button_guess.setOnClickListener {
             presenter.guess()
         }
     }
