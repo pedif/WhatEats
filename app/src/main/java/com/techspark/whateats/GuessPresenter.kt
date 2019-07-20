165cb01f366350c6178696c14eda35e8ee0931e6
Pedram <foroughi.pedram@gmail.com>
2019-07-20 09:55:38 +0300

    Update Guess Presenter
    Updated Guess Presenter to now stop any delayed calls when its stop function is called

--- app/src/main/java/com/techspark/whateats/GuessPresenter.kt
+++ app/src/main/java/com/techspark/whateats/GuessPresenter.kt
@@ -7,13 +7,15 @@ import android.os.Handler
 import kotlin.concurrent.schedule
 import kotlin.random.Random
 
-class GuessPresenter(private val view: View, private val context: Context) : Contract.Presenter {
+class GuessPresenter(private val view: View, context: Context) : Contract.Presenter {
 
     private val foods: Array<String> = context.resources.getStringArray(R.array.foods)
     private val formulas: Array<String> = context.resources.getStringArray(R.array.formulas)
 
 
     private val uiScope = Handler()
+
+    private var hasStopped = false
 
     /**
      * Picks a random number and selects the respective index of food array
@@ -22,7 +24,7 @@ class GuessPresenter(private val view: View, private val context: Context) : Con
 
         view.onStartGuessing()
         val rand = Random(System.currentTimeMillis())
-        guess(15,rand)
+        guess(15, rand)
     }
 
     /**
@@ -33,29 +35,34 @@ class GuessPresenter(private val view: View, private val context: Context) : Con
     /**
      * Recursive method which would display a formula @see{$formulaCount) times then displays the result and returns
      */
-     private fun guess(formulaCount: Int,rand: Random){
+    private fun guess(formulaCount: Int, rand: Random) {
 
-         if (count > formulaCount) {
-             count= 0
-             Timer().schedule(1000) {
-                 uiScope.post {
-                     view.guess(foods[rand.nextInt(0, foods.size)])
-                     view.onStopGuessing()
-                 }
-             }
-             return
-         }
-         count++
+        if (count > formulaCount) {
+            count = 0
+            Timer().schedule(1000) {
+                if(hasStopped)
+                    return@schedule
+                uiScope.post {
+                    view.guess(foods[rand.nextInt(0, foods.size)])
+                    view.onStopGuessing()
+                }
+            }
+            return
+        }
+        count++
 
-         Timer().schedule(300) {
-             uiScope.post {
-                 view.useFormula(formulas[rand.nextInt(0, formulas.size)])
-             }
-             guess(formulaCount,rand)
-         }
+        Timer().schedule(300) {
+            if(hasStopped)
+                return@schedule
+            uiScope.post {
+                view.useFormula(formulas[rand.nextInt(0, formulas.size)])
+            }
+            guess(formulaCount, rand)
+        }
     }
 
     override fun stop() {
+        hasStopped=true
     }
 
 
